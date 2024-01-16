1faf347e6bc36ee483f199dfb714a1f88ec5f210
MiguelM <up202108851@up.pt>
2024-01-16 04:39:24 +0000

    dc / kick / ban commands

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 from discord.ext import commands
 import discord
-import asyncio
 import os
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
--- cogs/admin.py
+++ cogs/admin.py
@@ -1,3 +1,4 @@
+from click import pass_context
 from discord.ext import commands
 import discord
 
@@ -6,6 +7,35 @@ class Admin (commands.Cog):
         self.bot = bot
 
         self.voice_clients = {}
+
+    @commands.command(name="disconnect", pass_context = True)
+    async def disconnect(self, ctx, member: discord.Member):
+        if ctx.message.author.guild_permissions.move_members:
+            if member.voice:
+                await member.move_to(None)
+                await ctx.send(f"{member.mention} has been kicked from the voice channel.")
+            else:
+                await ctx.send(member.mention + " is not in that voice channel.")
+        else:
+            await ctx.send("You don't have permissions to do that.")
+
+    @commands.command(name="kick", pass_context = True)
+    async def kick(self, ctx, member: discord.Member, *, reason:str):
+        if ctx.message.author.guild_permissions.kick_members:
+            await member.kick(reason=reason)
+            await ctx.send(f"{member.mention} was kicked from the server.")
+            await ctx.send("**Reason: **" + reason)
+        else:
+            await ctx.send("You don't have permissions to do that.")
+
+    @commands.command(name="ban", pass_context = True)
+    async def ban(self, ctx, member: discord.Member, *, reason:str):
+        if ctx.message.author.guild_permissions.ban_members:
+            await member.ban(reason=reason)
+            await ctx.send(f"{member.mention} was permanently banned from the server.")
+            await ctx.send("**Reason: **" + reason)
+        else:
+            await ctx.send("You don't have permissions to do that.")
 
 async def setup(bot):
     await bot.add_cog(Admin(bot))
